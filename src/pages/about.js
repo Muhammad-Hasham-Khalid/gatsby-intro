5405610a52d271ec8965981b22f64a249072b8f0
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-09-10 17:31:38 +0500

    pages and links

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>About Me</h1>
+    <p>THis is my personal website</p>
+    <Link to="/">&larr; back to home</Link>
+  </>
+);
