42bf6050cbf328e208622955c34a42c43365511a
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-09-12 16:17:49 +0500

    mdx blog

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout';
 export default () => (
   <Layout>
     <h1>About Me</h1>
-    <p>THis is my personal website</p>
+    <p>This is my personal website</p>
     <Link to="/">&larr; back to home</Link>
   </Layout>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/usePosts';
+import PostPreview from '../components/post-preview';
+export default () => {
+  const posts = usePosts();
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Minnesota!</p>
-    <Link to="/about">Learn about me &rarr;</Link>
-  </Layout>
-);
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Minnesota!</p>
+      <Link to="/about">Learn about me &rarr;</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
